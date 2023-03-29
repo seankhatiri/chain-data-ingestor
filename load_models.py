7237787774d7b206a85b726a17bca6640daf00f4
seankhatiri <hkhk.sharif@gmail.com>
2023-03-29 14:25:47 -0400

    clean up

--- load_models.py
+++ load_models.py
@@ -7,16 +7,6 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 model.eval()
 
-# *********************** Send the loaded model over HTTP ********************
-# import base64
-# from fastapi import FastAPI
-# app = FastAPI()
-# @app.get("/load_model")
-# async def load_model():
-#     model_data = base64.b64encode(pickle.dumps(model)).decode("utf-8")
-#     tokenizer_data = base64.b64encode(pickle.dumps(tokenizer)).decode("utf-8")
-#     return {"model": model_data, "tokenizer": tokenizer_data}
-
 r = redis.Redis(host='localhost', port=6379, db=0)
 model_data = pickle.dumps(model)
 tokenizer_data = pickle.dumps(tokenizer)
