b5e1f670ca4a87c7489006bfc59cce5a35d5e1d4
seankhatiri <hkhk.sharif@gmail.com>
2024-01-21 16:13:47 -0500

    removed transformers and torch to make the build more efficient

--- load_models.py
+++ load_models.py
@@ -1,14 +1,15 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-import pickle
-import redis
+# # Uncomment
+# from transformers import AutoTokenizer, AutoModelForSequenceClassification
+# import pickle
+# import redis
 
-model_name = 'sentence-transformers/paraphrase-distilroberta-base-v1'
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForSequenceClassification.from_pretrained(model_name)
-model.eval()
+# model_name = 'sentence-transformers/paraphrase-distilroberta-base-v1'
+# tokenizer = AutoTokenizer.from_pretrained(model_name)
+# model = AutoModelForSequenceClassification.from_pretrained(model_name)
+# model.eval()
 
-r = redis.Redis(host='localhost', port=6379, db=0)
-model_data = pickle.dumps(model)
-tokenizer_data = pickle.dumps(tokenizer)
-r.set("model", model_data)
-r.set("tokenizer", tokenizer_data)
+# r = redis.Redis(host='localhost', port=6379, db=0)
+# model_data = pickle.dumps(model)
+# tokenizer_data = pickle.dumps(tokenizer)
+# r.set("model", model_data)
+# r.set("tokenizer", tokenizer_data)
